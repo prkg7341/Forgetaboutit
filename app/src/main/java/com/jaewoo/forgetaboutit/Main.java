e09e1e8269adc2e4054d54fb5da8512e3dececd1
prkg7341 <40709295+prkg7341@users.noreply.github.com>
2018-08-01 10:42:32 +0900

    8.1 10:43

--- app/src/main/java/com/jaewoo/forgetaboutit/Main.java
+++ app/src/main/java/com/jaewoo/forgetaboutit/Main.java
@@ -14,16 +14,22 @@ public class Main extends Fragment {
 
     }
 
+    // fragment가 return될 때 실행되는 메소드
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        // 초기 화면을 "main"으로 설정
         final View view = inflater.inflate(R.layout.main, container, false);
+        // 새로고침 버튼 생성
         Button renew = (Button) view.findViewById(R.id.renew);
-        final TextView test = (TextView) view.findViewById(R.id.weather);
+        // 버튼 클릭시 내용을 출력할 TextView 생성 - 사용자의 위치를 출력하도록 수정예정
+        final TextView userLocation = (TextView) view.findViewById(R.id.userLocation);
 
+        // 버튼 클릭 리스너 생성 - 새로고침 기능으로 수정예정
         renew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                test.setText("The button works well!");
+                // 버튼 클릭시 userLocation TextView에 "The renew works well!" 메시지 출력 - 수정예정
+                userLocation.setText("The renew works well!");
             }
         });
         return view;
